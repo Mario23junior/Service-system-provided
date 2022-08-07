6872168a6797830c2e67d2107fdf7e36d90179d4
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-07 14:51:00 -0300

    Criando side-bar e adicionando component

--- src/app/templates/templates.module.ts
+++ src/app/templates/templates.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavBarComponent,
+    SidebarComponent
   ]
 })
 export class TemplatesModule { }
